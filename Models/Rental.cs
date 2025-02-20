28622e0a94d292d6dfd38e68a88a17913b5a60e7
TiagoMadeira <tiagopmadeira@gmail.com>
2025-02-20 12:55:28 +0000

    added Vehicle Rentals one to many relation

--- Models/Rental.cs
+++ Models/Rental.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,9 @@ namespace api.Models
         public int Id {get; set;}
         public int BlockedDateId {get; set;}
         public BlockedDate BlockedDate {get; set;}
+        public int VehicleId {get; set;}
+        [ForeignKey("RentalId")]
+        public Vehicle Vehicle {get; set;}
 
     }
 }
