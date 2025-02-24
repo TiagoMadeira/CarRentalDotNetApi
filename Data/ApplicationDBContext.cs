cfef7fb667ec65aa297b94236b908aa8178483de
TiagoMadeira <tiagopmadeira@gmail.com>
2025-02-24 16:15:54 +0000

    Added Identity roles User and Admin

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
@@ -21,6 +22,24 @@ namespace api.Data
         {
             modelBuilder.Conventions.Remove<CascadeDeleteConvention>();
             base.ConfigureConventions(modelBuilder);
+        }
+        protected override void OodelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
         }
         public  DbSet<Rental> Rentals {get; set;}
         public  DbSet<BlockedDate> BlockedDates {get; set;}
