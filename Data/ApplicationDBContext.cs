ded3cdc210e735fb60dd3e731eda882a186ccbc1
TiagoMadeira <tiagopmadeira@gmail.com>
2025-02-24 17:45:01 +0000

    Add register User endpoint

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -29,14 +29,18 @@ namespace api.Data
             List<IdentityRole> roles = new List<IdentityRole>
             {
                 new IdentityRole
-                {
+                {   
+                    Id = "5e438c8e-df84-4f39-80af-82306f2764f9",
                     Name = "Admin",
-                    NormalizedName = "ADMIN"
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = "6ac343b0-00ef-4a1c-8f64-68daaca77b5b"
                 },
                 new IdentityRole
-                {
+                {   
+                    Id = "a51a1291-d383-43f3-9522-cb9324885d11",
                     Name = "User",
-                    NormalizedName = "USER"
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = "1450449e-a75a-446c-9d00-866ed5f529f7"
                 }
             };
             builder.Entity<IdentityRole>().HasData(roles);
