8df3963affc024058316df17aa3b34e0cf1eea5b
TiagoMadeira <tiagopmadeira@gmail.com>
2025-02-26 17:26:08 +0000

    fixed token service not being initialized

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -23,6 +23,7 @@ namespace api.Controllers
         public AccountController(UserManager<AppUser> userManager, ITokenService tokenService , SignInManager<AppUser> signInManager) {
             _userManager = userManager;
             _signInManager = signInManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost("login")]
