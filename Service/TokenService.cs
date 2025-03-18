cb44c9d41d5685d32596c70a9ca8ecdef53c52b4
TiagoMadeira <tiagopmadeira@gmail.com>
2025-03-18 18:01:01 +0000

    bugfix: Fix jwt token claims declaration

--- Service/TokenService.cs
+++ Service/TokenService.cs
@@ -24,10 +24,10 @@ namespace api.Service
         {
            var claims = new List<Claim>
            {
-             new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
-             new Claim(JwtRegisteredClaimNames.Email, user.Email),
-             new Claim(JwtRegisteredClaimNames.GivenName, user.UserName)
-           };
+             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+             new Claim(ClaimTypes.Email, user.Email),
+             new Claim(ClaimTypes.GivenName, user.UserName)
+           };   
            
            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
